e12064e07d8d36e13129554ff9a30a1c4587bb27
Sophiewu22um <dc22886@um.edu.mo>
2026-03-18 18:19:22 +0800

    v6

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards','Writingsamples']
+const section_names = ['home', 'publications', 'awards','writingsamples']
 
 
 window.addEventListener('DOMContentLoaded', event => {
